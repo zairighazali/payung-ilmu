d12dbcfeb1ddd937b1f93055599415f5189c1817
zairighazali <151990419+zairighazali@users.noreply.github.com>
2025-12-14 17:36:18 +0800

    Update Quiz.jsx

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -8,20 +8,22 @@ function Quiz() {
   const nav = useNavigate();
 
   const click = (opt) => {
-    if (opt === quizData[index].answer) {
-      setScore(score + 1);
-    }
+    const isCorrect = opt === quizData[index].answer;
+    const newScore = isCorrect ? score + 1 : score;
+
+    setScore(newScore);
+
     if (index + 1 < quizData.length) {
       setIndex(index + 1);
     } else {
-      nav("/result", { state: { score } });
+      nav("/result", { state: { score: newScore } });
     }
   };
 
   return (
     <div className="container text-center p-5">
       <h3>{quizData[index].question}</h3>
-      {quizData[index].options.map(o => (
+      {quizData[index].options.map((o) => (
         <button
           key={o}
           className="btn btn-outline-primary m-2"
@@ -33,4 +35,5 @@ function Quiz() {
     </div>
   );
 }
+
 export default Quiz;
